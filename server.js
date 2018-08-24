ca51c1a771cac2d41fb0fb9fa107d7184a96f541
Richie Madrigal <richie.madrigal@icloud.com>
2018-08-23 21:49:47 -0400

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
 	extended: true
-}))
+}));
 
 app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({
