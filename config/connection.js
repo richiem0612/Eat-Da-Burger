b287cf2753f9c567fd894c571e1ecb61e19a2291
Richie Madrigal <richie.madrigal@icloud.com>
2018-09-19 12:51:00 -0400

    herokuConnection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ if(process.env.JAWSDB_URL){
 }else{
   connection = mysql.createConnection({
     host: "localhost",
-    port:8889,
     user: "root",
     password: "root",
     database: "burgers_db"
