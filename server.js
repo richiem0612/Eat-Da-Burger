2caa4126d6e68793143d24da26f6f8e2b821a68c
Richie Madrigal <richie.madrigal@icloud.com>
2018-09-19 12:25:23 -0400

    HerokuConnection

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
@@ -22,7 +23,7 @@ var routes = require("./controllers/routes.js");
 app.use('/', routes); // '/', 
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	// Log (server-side) when our server has started
 	console.log("Server listening on: http://localhost:" + PORT);
-  });
+});
